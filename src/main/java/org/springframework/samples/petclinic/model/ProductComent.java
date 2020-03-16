e86289f8aeae8617e92ba31cd576b47fc128f794
kiito <kiito@DESKTOP-P823TMC>
2020-03-16 19:58:15 +0100

    HU06 Pruebas

--- src/main/java/org/springframework/samples/petclinic/model/ProductComent.java
+++ src/main/java/org/springframework/samples/petclinic/model/ProductComent.java
@@ -1,5 +1,7 @@
+
 package org.springframework.samples.petclinic.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -7,51 +9,52 @@ import javax.persistence.ManyToOne;
 @Entity
 public class ProductComent extends Coment {
 
-    private Boolean highlight;
+	private Boolean	highlight;
 
-    private Integer rating;
+	private Integer	rating;
 
-    // USUARIO QUE HA PUBLICADO EL COMENTARIO/VALORACIÓN (PUEDE SER TANTO OWNER, VET
-    // O ADMIN)
-    @ManyToOne
-    @JoinColumn(name = "username")
-    private User user;
+	// USUARIO QUE HA PUBLICADO EL COMENTARIO/VALORACIÓN (PUEDE SER TANTO OWNER, VET
+	// O ADMIN)
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "username")
+	private User	user;
 
-    // PRODUCTO AL QUE SE LE HACE EL COMENTARIO/VALORACIÓN
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product;
+	// PRODUCTO AL QUE SE LE HACE EL COMENTARIO/VALORACIÓN
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "product_id")
+	private Product	product;
 
-    public Boolean getHighlight() {
-	return this.highlight;
-    }
 
-    public void setHighlight(final Boolean highlight) {
-	this.highlight = highlight;
-    }
+	public Boolean getHighlight() {
+		return this.highlight;
+	}
 
-    public Integer getRating() {
-	return this.rating;
-    }
+	public void setHighlight(final Boolean highlight) {
+		this.highlight = highlight;
+	}
 
-    public void setRating(final Integer rating) {
-	this.rating = rating;
-    }
+	public Integer getRating() {
+		return this.rating;
+	}
 
-    public User getUser() {
-	return this.user;
-    }
+	public void setRating(final Integer rating) {
+		this.rating = rating;
+	}
 
-    public void setUser(final User user) {
-	this.user = user;
-    }
+	public User getUser() {
+		return this.user;
+	}
 
-    public Product getProduct() {
-	return this.product;
-    }
+	public void setUser(final User user) {
+		this.user = user;
+	}
 
-    public void setProduct(final Product product) {
-	this.product = product;
-    }
+	public Product getProduct() {
+		return this.product;
+	}
+
+	public void setProduct(final Product product) {
+		this.product = product;
+	}
 
 }
