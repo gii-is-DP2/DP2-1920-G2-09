51dbf9f60be4aae0d1aa09399fe1405e2b11fbef
Francisco Javier Romero González-Regueral <56021961+fraromgon2us@users.noreply.github.com>
2020-03-27 09:59:50 +0100

    Cambios para el integración con TravisCI

--- src/main/java/org/springframework/samples/petclinic/model/ProductComent.java
+++ src/main/java/org/springframework/samples/petclinic/model/ProductComent.java
@@ -8,51 +8,51 @@ import javax.persistence.ManyToOne;
 @Entity
 public class ProductComent extends Coment {
 
-    private Boolean highlight;
+	private Boolean highlight;
 
-    private Integer rating;
+	private Integer rating;
 
-    // USUARIO QUE HA PUBLICADO EL COMENTARIO/VALORACIÓN (PUEDE SER TANTO OWNER, VET
-    // O ADMIN)
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "username")
-    private User user;
+	// USUARIO QUE HA PUBLICADO EL COMENTARIO/VALORACIÓN (PUEDE SER TANTO OWNER, VET
+	// O ADMIN)
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "username")
+	private User user;
 
-    // PRODUCTO AL QUE SE LE HACE EL COMENTARIO/VALORACIÓN
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "product_id")
-    private Product product;
+	// PRODUCTO AL QUE SE LE HACE EL COMENTARIO/VALORACIÓN
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "product_id")
+	private Product product;
 
-    public Boolean getHighlight() {
-	return this.highlight;
-    }
+	public Boolean getHighlight() {
+		return this.highlight;
+	}
 
-    public void setHighlight(final Boolean highlight) {
-	this.highlight = highlight;
-    }
+	public void setHighlight(final Boolean highlight) {
+		this.highlight = highlight;
+	}
 
-    public Integer getRating() {
-	return this.rating;
-    }
+	public Integer getRating() {
+		return this.rating;
+	}
 
-    public void setRating(final Integer rating) {
-	this.rating = rating;
-    }
+	public void setRating(final Integer rating) {
+		this.rating = rating;
+	}
 
-    public User getUser() {
-	return this.user;
-    }
+	public User getUser() {
+		return this.user;
+	}
 
-    public void setUser(final User user) {
-	this.user = user;
-    }
+	public void setUser(final User user) {
+		this.user = user;
+	}
 
-    public Product getProduct() {
-	return this.product;
-    }
+	public Product getProduct() {
+		return this.product;
+	}
 
-    public void setProduct(final Product product) {
-	this.product = product;
-    }
+	public void setProduct(final Product product) {
+		this.product = product;
+	}
 
 }
