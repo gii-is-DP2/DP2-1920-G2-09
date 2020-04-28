3d3ae83f632010bffe02785fe720d198cbbc7811
Francisco Javier Romero González-Regueral <56021961+fraromgon2us@users.noreply.github.com>
2020-04-28 09:00:24 +0200

    Corrección Pruebas de Integración con DB

--- src/test/java/org/springframework/samples/petclinic/service/PetServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/PetServiceTests.java
@@ -26,6 +26,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.model.Owner;
@@ -70,6 +71,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PetServiceTests {
 	@Autowired
 	protected PetService petService;
--- src/test/java/org/springframework/samples/petclinic/service/VetServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/VetServiceTests.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.DataAccessException;
@@ -67,6 +68,7 @@ import org.springframework.stereotype.Service;
  */
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class VetServiceTests {
 
 	@Autowired
